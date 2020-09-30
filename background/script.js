19f0bab63fd124e358071eaf6a758629978c0bc8
koowoy <uk960214@gmail.com>
2020-09-30 11:59:09 +0000

    upgrade background generator

---
+++ background/script.js
@@ -0,0 +1,26 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector(".random");
+
+function pickRandomColor(e) {
+  color1.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+  color2.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+  setGradient();
+}
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value
+	+ ")";
+	css.textContent = body.style.background + ";";
+}
+
+window.onload = pickRandomColor;
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+random.addEventListener("click", pickRandomColor);
